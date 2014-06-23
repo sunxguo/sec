e992c6cd557ef227d7abd685504fdfa28a57d25f
JackSun <sunxguo@163.com>
2014-06-24 01:15:36 +0800

    resize img

--- application/controllers/cuc.php
+++ application/controllers/cuc.php
@@ -626,7 +626,8 @@ class Cuc extends CI_Controller {
 		return  count($merchant)>0?$merchant[0]:array();
 	}
 	public function traverse() {
-		$current_dir = opendir("/var/www/xc/uploads/image");    //opendir()返回一个目录句柄,失败返回false
+		$path="/var/www/xc/uploads/image";
+		$current_dir = opendir($path);    //opendir()返回一个目录句柄,失败返回false
 		while(($file = readdir($current_dir)) !== false) {    //readdir()返回打开目录句柄中的一个条目
 			$sub_dir = $path . DIRECTORY_SEPARATOR . $file;    //构建子目录路径
 			if($file == '.' || $file == '..') {
